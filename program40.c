c1c4a6cec32325bd31b5cd3d48beba6bb9ddc954
Suprio Adhikary <suprioadhikary802@gmail.com>
2026-09-21 22:46:43 +0530

    optimized code 40

--- program40.c
+++ program40.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
   int n, alpha_count = 0, digit_count = 0, special_count = 0;
-  printf("\nEnter password length : ");
-  scanf("%d", &n);
-  char password[n];
-  printf("\nEnter password :\n");
-  for (int i = 0; i < n; i++)
-    scanf(" %c", &password[i]);
-  for (int i = 0; i < n; i++)
+  char password[100];
+  printf("Enter password : ");
+  fgets(password, 100, stdin);
+  for (int i = 0; password[i] != '\0' && password[i] != '\n'; i++)
   {
     if ((password[i] >= 'A' && password[i] <= 'Z') || (password[i] >= 'a' && password[i] <= 'z'))
       alpha_count++;
@@ -17,13 +15,13 @@ int main()
     else
       special_count++;
   }
-  if (n < 6)
+  if (strlen(password) - 1 < 6)
     printf("\nWeak Password !");
-  if ((n >= 6 && n <= 10) && alpha_count != 0 && digit_count == 0 && special_count == 0)
+  if ((strlen(password) - 1 >= 6 && strlen(password) - 1 <= 10) && alpha_count != 0 && digit_count == 0 && special_count == 0)
     printf("\nModerate Password !");
-  if ((n >= 6 && n <= 10) && alpha_count != 0 && digit_count != 0 && special_count == 0)
+  if ((strlen(password) - 1 >= 6 && strlen(password) - 1 <= 10) && alpha_count != 0 && digit_count != 0 && special_count == 0)
     printf("\nStrong Password !");
-  if ((n > 10) && alpha_count != 0 && digit_count != 0 && special_count != 0)
+  if ((strlen(password) - 1 > 10) && alpha_count != 0 && digit_count != 0 && special_count != 0)
     printf("\nVery Strong Password !");
   return 0;
 }
